e17210957d5adf03a3978a2ce5b3821c3a8a2112
shahkalpan <118063572+shahkalpan@users.noreply.github.com>
2023-01-26 17:41:21 +1000

    PostgreSQL API

--- docker/app/main.py
+++ docker/app/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/connectpostgresql")
+async def root():
+    return {"message": "Connection to PostgreSQL successful"}
