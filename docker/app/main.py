9b66699e36daae693acf7fd0baee5a7782fb48f5
shahkalpan <118063572+shahkalpan@users.noreply.github.com>
2023-01-26 17:46:02 +1000

    Update main.py

--- docker/app/main.py
+++ docker/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import psycopg2
 
 app = FastAPI()
 
@@ -8,4 +9,16 @@ async def root():
 
 @app.get("/connectpostgresql")
 async def root():
-    return {"message": "Connection to PostgreSQL successful"}
+    for i in range(retries):
+        print('Connect attempt' + str(i))
+        try:
+            conn = psycopg2.connect("host=db dbname='postgres' user='postgres' password='postgres'")
+            print('Connection successful')
+            return {"message": "Connection to PostgreSQL successful"}
+            break
+        except psycopg2.OperationalError:
+            print('Connection failed')
+            sleep(delay)
+            conn = None
+            return {"message": "Not able to connect PostgreSQL"}
+    
