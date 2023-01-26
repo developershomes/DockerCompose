1e70e763f6f204636c271691784f34899b2e4d59
shahkalpan <118063572+shahkalpan@users.noreply.github.com>
2023-01-26 17:50:22 +1000

    Final update

--- docker/app/main.py
+++ docker/app/main.py
@@ -9,16 +9,12 @@ async def root():
 
 @app.get("/connectpostgresql")
 async def root():
-    for i in range(retries):
-        print('Connect attempt' + str(i))
-        try:
-            conn = psycopg2.connect("host=db dbname='postgres' user='postgres' password='postgres'")
-            print('Connection successful')
-            return {"message": "Connection to PostgreSQL successful"}
-            break
-        except psycopg2.OperationalError:
-            print('Connection failed')
-            sleep(delay)
-            conn = None
-            return {"message": "Not able to connect PostgreSQL"}
+    try:
+        conn = psycopg2.connect("host=db dbname='postgres' user='postgres' password='postgres'")
+        print('Connection successful')
+        return {"message": "Connection to PostgreSQL successful"}
+    except psycopg2.OperationalError:
+        print('Connection failed')
+        conn = None
+        return {"message": "Not able to connect PostgreSQL"}
     
